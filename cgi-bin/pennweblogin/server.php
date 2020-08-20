c81a2c3ff16d745efbd5e437592d252fa585b5a7
Catherine Liang <catherineliang39@gmail.com>
2020-08-20 17:07:40 -0400

    Added cgi & php for internal discussion

---
+++ cgi-bin/pennweblogin/server.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PHP $_SERVER</title>
+</head>
+<body>
+<h1>PHP $_SERVER</h1>
+<p>Contents of $_SERVER: Server and execution environment information</p>
+<pre>
+<?php
+echo "printing server.php contents: ";
+print_r($_SERVER);
+?>
+</pre>
+</body>
+</html>
+
