048ec92ee785042de0c317f015c73cbd3a611071
Catherine Liang <catherineliang39@gmail.com>
2020-08-20 21:21:23 -0400

    Added connection test, changed structure to protect reading gp

---
+++ cgi-bin/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "alliance.seas.upenn.edu";
+$dbUsername = "am2020";
+$dbPassword = "precise2020";
+$dbName = "am2020";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
