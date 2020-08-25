ac9f6f8439dd2f179413221f7b5331da7a3a2da1
Catherine Liang <catherineliang39@gmail.com>
2020-08-25 16:14:01 -0400

    Added responsive scaling & mobile menu

--- cgi-bin/internal-discussion/index.php
+++ cgi-bin/internal-discussion/index.php
@@ -29,14 +29,50 @@
     </style>
 
     <body>
-
+        <div class="d-block d-lg-none">
+            <nav class="mobile-menu navbar navbar-dark fixed-top">
+                <a class="navbar-brand" href="/index.html">
+                    <img src="images/header-mobile.png" style="height: 20px; width: auto;">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/overview.html" class="nav-selected display-4">Project Overview</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/people.html">People</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Paper</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/events.html">Meetings/Events</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/cgi-bin/reading-group.html">Reading Group</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/cgi-bin/internal-discussion">Internal Discussion</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
         <div class="page-grid">
             <img class="header-bg" src="/images/title-bg.png" />
             <img class="header-neural1" src="/images/child-graphic-02.png" />
             <img class="header-neural2" src="/images/child-graphic-13-gradient.png" />
             <img class="header-shield" src="/images/logo-final.png" />
-            <a class="header-title" href="/index.html"><img src="/images/title-block-no-bg.png" /></a>
-            <div class="nav-container">
+            <span class="header-title d-none d-lg-block"><a href="index.html"><img src="/images/title-block-no-bg.png" /></a></span>
+            <span class="header-title d-block d-lg-none"><a href="index.html"><img src="/images/header-title.png" /></a></span>
+            <div class="d-none d-lg-block nav-container">
                 <nav>
                     <ul>
                         <li><a href="/overview.html">Project Overview</a></li>
@@ -146,5 +182,10 @@
                 </div>
             </div>
         </footer>
+
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
