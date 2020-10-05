3eb63ea6bad46e5c322639eca61fb352b5215e03
qingyuanpeng <pqy@seas.upenn.edu>
2020-10-05 17:04:02 -0400

    add access

--- cgi-bin/includes/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbServername = "alliance.seas.upenn.edu";
-$dbUsername = "am2020";
-$dbPassword = "precise2020";
-$dbName = "am2020";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) 
-OR die('Could not connect to MySQL. Error: ' . mysqli_connect_error());
